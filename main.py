46cad0d904e189310d8b5f9395236463a63faea1
LagoonyToons <35708013+LagoonyToons@users.noreply.github.com>
2018-07-20 23:57:19 -0400

    Update main.py
    shoulder_m is now labeled as arm_m and the previous arm_m is now wrist_m.

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ class Robot():
     def __init__(self):
         self.base_m     = vex.Motor(2)
         self.claw_m     = vex.Motor(6) 
-        self.shoulder_m = vex.Motor(3)
-        self.arm_m      = vex.Motor(7)
+        self.arm_m      = vex.Motor(3)
+        self.wrist_m    = vex.Motor(7)
         self.joystick   = vex.Joystick()
         
         
@@ -40,17 +40,17 @@ class Robot():
             
             #shoulder
             if self.joystick.b7up() is False and self.joystick.b7down() is False:
-                self.shoulder_m.off()
-            elif self.joystick.b7up():
-                self.shoulder_m.run(-30)
-            elif self.joystick.b7down():
-                self.shoulder_m.run(30)
-                
-            #arm
-            if self.joystick.axis2() != 0:
-                self.arm_m.run(self.joystick.axis2())
-            elif self.joystick.axis2() == 0:
                 self.arm_m.off()
+            elif self.joystick.b7up():
+                self.arm_m.run(-30)
+            elif self.joystick.b7down():
+                self.arm_m.run(30)
+                
+            #wrist
+            if self.joystick.axis2() != 0:
+                self.wrist_m.run(self.joystick.axis2())
+            elif self.joystick.axis2() == 0:
+                self.wrist_m.off()
                 
             #claw
             if self.joystick.b8up() is False and self.joystick.b8down() is False:
