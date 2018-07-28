10e12e2a3d59b9e135ad9e9485896db3048d16cf
Atikul10152002 <Atikul10152002@gmail.com>
2018-07-28 02:21:44 -0400

    codedocs

---
+++ vex.py
@@ -0,0 +1,32 @@
+## file
+class Motor:
+	def __init__(self, *a, **k):
+		pass
+	def run(self, *a, **k):
+		pass
+
+	def off(self, *a, **k):
+		pass
+
+
+class Joystick:
+	def __init__(self, *a, **k):
+		pass
+	def axis4(self, *a, **k):
+		return 0
+		
+	def axis2(self, *a, **k):
+		return 0
+
+	def b7up(self, *a, **k):
+		return 0
+
+	def b7down(self, *a, **k):
+		return 0
+
+	def b8up(self, *a, **k):
+		return 0
+
+	def b8down(self, *a, **k):
+		return 0
+		
